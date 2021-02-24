67737856eca97137ff4c8145d1b27f06575382b0
hieptran1812 <44777689+hieptran1812@users.noreply.github.com>
2021-02-24 20:53:29 +0700

    fix

--- startapp.sh
+++ startapp.sh
@@ -1,5 +1,7 @@
 eval "$(conda shell.bash hook)"
+# active virtual environment
 source /home/namvh/PTIT-IOT-Healthcare/venv/bin/active
+# run app.py
 python app.py &
 # /snap/bin/ngrok http -subdomain=ptit03 8000 &
 /snap/bin/ngrok http -subdomain=ptit03 8000 > /dev/null &

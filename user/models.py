5940e35373784cfa8038228a276fef1eef180dd3
hieptran1812 <44777689+hieptran1812@users.noreply.github.com>
2021-04-06 01:16:03 +0700

    up

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
-from passlib.hash import pbkdf2_sha256
+# from passlib.hash import pbkdf2_sha256
 from flask_pymongo import pymongo
 import pandas as pd
 

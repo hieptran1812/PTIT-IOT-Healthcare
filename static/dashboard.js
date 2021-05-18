34977bfe238e6735012d3b11b67412782753a655
hieptran1812 <44777689+hieptran1812@users.noreply.github.com>
2021-05-18 16:16:30 +0700

    update

--- static/dashboard.js
+++ static/dashboard.js
@@ -6,10 +6,12 @@
   var admin = 0;
   var doctor = 0;
   var patient = 0;
+  var famPatient = 0;
   var roles = document.getElementsByClassName("role");
   for (var i = 0; i < roles.length; i++) {
     if (roles[i].innerText === "Admin") admin++;
     else if (roles[i].innerText === "Bác sĩ") doctor++;
+    else if (roles[i].innerText === "Người nhà bệnh nhân") famPatient++;
     else patient++;
   }
 
@@ -17,14 +19,15 @@
   var myChart = new Chart(ctx, {
     type: "pie",
     data: {
-      labels: ["Bệnh nhân", "Bác sĩ", "Admin"],
+      labels: ["Bệnh nhân", "Bác sĩ", "Admin", "Người nhà bệnh nhân"],
       datasets: [
         {
-          data: [patient, doctor, admin],
+          data: [patient, doctor, admin, famPatient],
           backgroundColor: [
             "rgb(255, 99, 132)",
             "rgb(35, 193, 120)",
-            "rgb(54, 162, 235)"
+            "rgb(54, 162, 235)",
+            "rgb(133, 212, 171)",
           ],
         },
       ],
